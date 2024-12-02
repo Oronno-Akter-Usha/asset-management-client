5783568ddeb005b836f1cc9bfa734f2eba1f100f
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-12-02 20:58:15 +0600

    feat: add useAuth

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
+import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-screen-2xl h-screen mx-auto bg-[#f5f5f5]">
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className="max-w-screen-2xl h-screen mx-auto bg-[#f5f5f5]">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>
 );
